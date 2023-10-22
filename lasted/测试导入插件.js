525e37042c14d4716aaf65476b9e7bcd19a52191
樱川流夏立 <1312552724@qq.com>
2023-10-22 20:27:21 +0800

    Create 测试导入插件.js

---
+++ lasted/测试导入插件.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        测试导入插件
+// @namespace   f1abec8c-c256-4c7a-aeb3-310a7d1b677b
+// @version     0.0.1
+// @author      hunmer
+// @description 注释说明
+// @updateURL               
+// @primary     1
+// ==/UserScript==
+
+alert('成功加载测试导入插件')
