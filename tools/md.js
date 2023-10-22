4c098412f807afa78723466ea4d75d1a5c07f573
樱川流夏立 <1312552724@qq.com>
2023-10-22 21:51:06 +0800

    1

--- tools/md.js
+++ tools/md.js
@@ -1,10 +1,10 @@
 const {readdirSync, writeFileSync} = require('fs')
 
-let output = '#点击下面链接可在线导入 \r\n\r\n'
+let output = '# 点击下面链接可在线导入 \r\n\r\n'
 readdirSync('../lasted/').forEach(fileName => {
     if(fileName.endsWith('.js')){
-        output += `-   [${fileName}](mlauncher:\\\\?appId=mCollection&method=doMethod&data=${JSON.stringify({method: 'plugin_import', data: {url: 'https://github.com/hunmer/mCollection-scripts/raw/main/lasted/'+fileName}})})\r\n`
+        output += `- [${fileName}](mlauncher:\\\\?appId=mCollection&method=doMethod&data=${JSON.stringify({method: 'plugin_import', data: {url: 'https://github.com/hunmer/mCollection-scripts/raw/main/lasted/'+fileName}})})\r\n`
     }
 })
 
-writeFileSync('../README', output)
+writeFileSync('../README.md', output)
